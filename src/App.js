1bb14205868fb81ae4d6ce2e1ad18adcb300ed3a
0RC4 <wjdwldn1121@gmail.com>
2024-09-23 17:26:20 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Amplify } from 'aws-amplify';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import awsExports from './aws-exports';
+import '@aws-amplify/ui-react/styles.css';
+
+Amplify.configure(awsExports);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to Amplify Auth App</h1>
+        <p>This is a sample authentication app using Amazon Cognito.</p>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
